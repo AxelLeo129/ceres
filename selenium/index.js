1ecbe790ec8ec033a7a21ff7d2d0d833b63b495c
AxelLeo129 <axelleo129dev@gmail.com>
2022-11-07 13:53:17 -0600

    Selenium.

---
+++ selenium/index.js
@@ -0,0 +1,17 @@
+const { By, Key, Builder } = require('selenium-webdriver');
+require('chromedriver')
+
+async function test_case() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    try {    
+        await driver.get('http://localhost:8100/login');
+        await driver.findElement(By.name('ion-input-0')).sendKeys('axel@email.com', Key.RETURN);
+        await driver.findElement(By.name('ion-input-1')).sendKeys('Axel.129', Key.RETURN);
+        await driver.findElement(By.id('login')).click();
+    } finally {
+        await driver.quit();
+    }
+
+}
+
+test_case();
