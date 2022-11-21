6a30a5bad5af7391f1a968f88b10d78157e712f7
AxelLeo129 <axelleo129dev@gmail.com>
2022-11-21 14:29:08 -0600

    Fixed build folder & selenium

--- selenium/index.js
+++ selenium/index.js
@@ -4,7 +4,7 @@ require('chromedriver')
 async function test_case() {
     let driver = await new Builder().forBrowser("chrome").build();
     try {    
-        await driver.get('http://localhost:8100/login');
+        await driver.get('https://ceres-7e3af.firebaseapp.com/login');
         await driver.findElement(By.name('ion-input-0')).sendKeys('axel@email.com', Key.RETURN);
         await driver.findElement(By.name('ion-input-1')).sendKeys('Axel.129', Key.RETURN);
         await driver.findElement(By.id('login')).click();
